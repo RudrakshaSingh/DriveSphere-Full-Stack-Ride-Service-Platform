2db47e6bffb857759eb8e32ece5633903913afd4
Thakur Rudraksha Singh <rudrakshasingh@gmail.com>
2025-02-01 21:55:08 +0530

    use signup changed to form data

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -5,13 +5,16 @@ const blackListTokenModel = require("../models/blackListToken.model");
 
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
+  
 
   //if something if wrong the give error in routes comes in errors.array
   if (!errors.isEmpty()) {
+    console.log("fff");
+    
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const { fullname, email, password } = req.body;
+  const { firstname,lastname, email, password } = req.body;
 
   const isUserAlreadyExists = await userModel.findOne({ email });
   if (isUserAlreadyExists) {
@@ -21,8 +24,8 @@ module.exports.registerUser = async (req, res, next) => {
   const hashedPassword = await userModel.hashPassword(password);
 
   const user = await userService.createUser({
-    firstname: fullname.firstname,
-    lastname: fullname.lastname,
+    firstname: firstname,
+    lastname: lastname,
     email,
     password: hashedPassword,
   });
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,25 +2,27 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const userController = require("../controllers/user.controller");
-const authMiddleware = require('../middlewares/auth.middleware');
+const authMiddleware = require("../middlewares/auth.middleware");
+const upload = require("../middlewares/multer.middleware");
 
 router.post(
-  "/register",
-  [
-    body("email").isEmail().withMessage("Invalid Email"),
-    body("fullname.firstname").isLength({ min: 3 }).withMessage("First name must be at least 3 characters long"),
-    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
-  ],
-  userController.registerUser
+	"/register",upload.none(),
+	[
+		body("email").isEmail().withMessage("Invalid Email"),
+		body("firstname").isLength({ min: 3 }).withMessage("First name must be at least 3 characters long"),
+		body("lastname").isLength({ min: 3 }).withMessage("Last name must be at least 3 characters long"),
+		body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
+	],
+	userController.registerUser
 );
 
 router.post(
-  "/login",
-  [
-    body("email").isEmail().withMessage("Invalid Email"),
-    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
-  ],
-  userController.loginUser
+	"/login",
+	[
+		body("email").isEmail().withMessage("Invalid Email"),
+		body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
+	],
+	userController.loginUser
 );
 
 router.get("/profile", authMiddleware.authUser, userController.getUserProfile);
